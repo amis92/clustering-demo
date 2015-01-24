b650daf788878e00d03d39f0c9f79d2ab4a8cc61
Amadeusz Sadowski <amis.tau.92@gmail.com>
2015-01-24 19:44:23 +0100

    cmeans implemented

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -5,115 +5,141 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
 
+import clusterer.FCMPointsClusterer;
 import clusterer.FlameClusterer;
 import clusterer.Point;
 
 @SuppressWarnings("serial")
-public class GUI extends JFrame
-{	
-	private FlameClusterer flameClusterer;
-	private ArrayList<Point> points = new ArrayList<>();
-	private final MouseListener mouseListener = new MouseListener() {
-		@Override
-		public void mouseClicked(MouseEvent arg0) {
-			points.add(new Point(arg0.getXOnScreen()-0, arg0.getYOnScreen() -28));
-			drawingPanel.repaint();
-		}
-		@Override
-		public void mouseEntered(MouseEvent arg0) {}
-		@Override
-		public void mouseExited(MouseEvent arg0) {}
-		@Override
-		public void mousePressed(MouseEvent arg0) {}
-		@Override
-		public void mouseReleased(MouseEvent arg0) {}
-	};
-	
-	private final SomePanel drawingPanel = new SomePanel();
+public class GUI extends JFrame {
 	class SomePanel extends JPanel implements ActionListener {
-		
-
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
-
 		}
-		
 		@Override
-		protected void paintComponent(Graphics g){
-			super.paintComponent(g);		
-			Graphics2D g2d = (Graphics2D)g;
-			for(Point point: points) {
-					g2d.setPaint(point.color);
-					g2d.fill(point.graphicalPoint);
+		protected void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			Graphics2D g2d = (Graphics2D) g;
+			for (Point point : points) {
+				g2d.setPaint(point.color);
+				g2d.fill(point.graphicalPoint);
 			}
 			repaint();
 		}
-		
+	}
+	private final SomePanel drawingPanel = new SomePanel();
+	private int expectedClusters = 6;
+	private FlameClusterer flameClusterer = null;
+	private FCMPointsClusterer fmcClusterer = null;
+	private double fuzzifier = 2.0;
+
+	private final MouseAdapter mouseListener = new MouseAdapter() {
+		@Override
+		public void mouseClicked(MouseEvent arg0) {
+			points.add(new Point(arg0.getXOnScreen() - 0,
+					arg0.getYOnScreen() - 28));
+			drawingPanel.repaint();
+		}
 	};
 
-	
-	
-	public GUI()
-	{
-		
+	private ArrayList<Point> points = new ArrayList<>();;
+
+	public GUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		this.addKeyListener(new KeyListener() {
-
+		this.addKeyListener(new KeyAdapter() {
 			@Override
-			public void keyPressed(KeyEvent arg0) {
-				if(arg0.getKeyCode() == KeyEvent.VK_SPACE) {
-					points.clear();
-				}
-				else if(arg0.getKeyCode() == KeyEvent.VK_F){
-					try {
-						flameClusterer = new FlameClusterer(points, 15, 100);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-					flameClusterer.execute();
-				}
-				else if(arg0.getKeyCode() == KeyEvent.VK_C){	//miejsce na fuzzy c means
-					
-				}
-				drawingPanel.repaint();
+			public void keyPressed(KeyEvent e) {
+				GUI.this.keyPressed(e);
 			}
-
-			@Override
-			public void keyReleased(KeyEvent arg0) {
-				
-			}
-
-			@Override
-			public void keyTyped(KeyEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-			
 		});
-		
 		this.setPreferredSize(new Dimension(1280, 768));
 		this.addMouseListener(mouseListener);
-		
 		add(drawingPanel);
-		//add(iterationsSlider, BorderLayout.EAST);
-		
-		
+		// add(iterationsSlider, BorderLayout.EAST);
 		pack();
 		setVisible(true);
 	}
-	
-	public void setPointSet(ArrayList<Point> points){
+
+	private void getNewFmcParams() {
+		final JSpinner cField = new JSpinner(new SpinnerNumberModel(
+				expectedClusters, 2, Integer.MAX_VALUE, 1));
+		final JSpinner mField = new JSpinner(new SpinnerNumberModel(fuzzifier,
+				1.001, 100.0, 0.001));
+		final Object[] params = {
+				new JLabel("Number of clusters in Fuzzy C Means:"), cField,
+				new JLabel("Fuzzying parameter (any real > 1):"), mField };
+		// show panel
+		final int d = JOptionPane.showConfirmDialog(drawingPanel, params,
+				"Fuzzy C Means parameters:", JOptionPane.OK_CANCEL_OPTION);
+		// show confirmation and set params
+		if (d == 0) {
+			expectedClusters = (Integer) cField.getValue();
+			fuzzifier = (Double) mField.getValue();
+			String message = String.format("New parameters:\n"
+					+ "Number of clusters in Fuzzy C Means: = %d\n"
+					+ "Fuzzying parameter (any real > 1): = %f\n",
+					expectedClusters, fuzzifier);
+			JOptionPane.showMessageDialog(null, message);
+		}
+	}
+
+	private void keyPressed(KeyEvent e) {
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_SPACE:
+			points.clear();
+			break;
+		case KeyEvent.VK_F:
+			runFlame();
+			break;
+		case KeyEvent.VK_C:
+			runFuzzyC();
+			break;
+		case KeyEvent.VK_S:
+			newFmcClusterer();
+			break;
+		case KeyEvent.VK_R:
+			getNewFmcParams();
+			break;
+		}
+		drawingPanel.repaint();
+	}
+
+	private void newFmcClusterer() {
+		fmcClusterer = new FCMPointsClusterer(points, expectedClusters,
+				fuzzifier);
+	}
+
+	private void runFlame() {
+		try {
+			flameClusterer = new FlameClusterer(points, 15, 100);
+			flameClusterer.execute();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	private void runFuzzyC() {
+		try {
+			newFmcClusterer();
+			fmcClusterer.execute();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public void setPointSet(ArrayList<Point> points) {
 		this.points = points;
 	}
-	
 }
